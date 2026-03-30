51ecae9fddc84dfe5e4d7b35769d384e6357ee96
gopikiranspam <gopikiranspam@gmail.com>
2026-03-30 18:45:29 +0530

    feat: Update sitemap to use domain and dynamic lastmod
    This change updates the sitemap generation to use the correct domain "https://toletbro.com" instead of a development URL. It also dynamically sets the `lastmod` date for each URL to the current date, ensuring sitemaps are always up-to-date for search engine crawlers. New URLs for privacy policy and terms of service have also been added.

--- server.ts
+++ server.ts
@@ -16,31 +16,51 @@ async function startServer() {
   });
 
   app.get("/sitemap.xml", (req, res) => {
+    const baseUrl = "https://toletbro.com";
+    const today = new Date().toISOString().split('T')[0];
+    
     res.header('Content-Type', 'application/xml');
     res.send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://toletbro.com/</loc>
+    <loc>${baseUrl}/</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://toletbro.com/search/hyderabad</loc>
+    <loc>${baseUrl}/search/hyderabad</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://toletbro.com/scan</loc>
-    <changefreq>monthly</changefreq>
+    <loc>${baseUrl}/scan</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://toletbro.com/about-us</loc>
+    <loc>${baseUrl}/about-us</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
-    <priority>0.5</priority>
+    <priority>0.6</priority>
   </url>
   <url>
-    <loc>https://toletbro.com/list-property</loc>
+    <loc>${baseUrl}/privacy-policy</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/terms-of-service</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/list-property</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
